c1ce25c92e98f092cf355c1a1824c1634d7e9f31
Melkia44 <melkiabz@gmail.com>
2025-11-07 12:06:31 +0100

    feat: finalisation du pipeline Docker + ingestion Mongo automatisée

--- MLO_P5_Sources/src/config.py
+++ MLO_P5_Sources/src/config.py
@@ -1,10 +1,23 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+# Nom de la base et de la collection
+DB_NAME = os.getenv("APP_DB", "MLO_DE_Projet5")
+COLL_NAME = os.getenv("COLL_NAME", "patients")
 
-MONGODB_URI = os.getenv("MONGODB_URI", "mongodb://localhost:27017")
-DB_NAME = os.getenv("MONGODB_DB", "healthcare")
-COLL_NAME = os.getenv("MONGODB_COLLECTION", "admissions")
-CSV_PATH = os.getenv("CSV_PATH", "./Data/healthcare_dataset.csv")
-BATCH_SIZE = int(os.getenv("BATCH_SIZE", "5000"))
+# URI Mongo : on utilise le service Docker "mongo"
+APP_USER = os.getenv("APP_USER")
+APP_PWD = os.getenv("APP_PWD")
+MONGO_HOST = os.getenv("MONGO_HOST", "mongo")
+MONGO_PORT = os.getenv("MONGO_PORT", "27017")
+
+MONGODB_URI = os.getenv(
+    "MONGODB_URI",
+    f"mongodb://{APP_USER}:{APP_PWD}@{MONGO_HOST}:{MONGO_PORT}/{DB_NAME}?authSource=admin"
+)
+
+# Chemin vers le CSV dans le conteneur
+CSV_PATH = os.getenv("CSV_PATH", "/data/csv/healthcare_dataset.csv")
+
+# Taille des batchs pour l'insert
+BATCH_SIZE = int(os.getenv("BATCH_SIZE", "1000"))
+
